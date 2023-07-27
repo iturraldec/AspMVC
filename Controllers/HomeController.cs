1444be81f5355748813459071e1b573df888ef27
Carlos Iturralde <iturraldec@gmail.com>
2023-07-27 11:46:34 -0500

    Inicio de proyecto AspMVC [master]

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspMVC.Models;
+
+namespace AspMVC.Controllers;
+
+public class HomeController : Controller
+{
+    public HomeController()
+    {
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
